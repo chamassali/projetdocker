7078ecfbae66309317545aa560a9759bc715d618
Ali Chamass <60328006+chamassali@users.noreply.github.com>
2021-05-31 18:07:47 +0200

    Liste des profils + route test/id/Nom-Prenom

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,20 +2,30 @@
 
 namespace App\Controller;
 
+use App\Entity\Profile;
 use App\Repository\CitiesRepository;
 use App\Repository\ProfileRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     /**
      *@Route("/", name="home", methods={"GET"})
      */
-    public function index(CitiesRepository $repo): Response
+    public function index(ProfileRepository $repo): Response
     {
-        $cities = $repo->findAll();
-        return $this->render('home/index.html.twig', compact('cities'));
+        $profiles = $repo->findAll();
+        return $this->render('home/index.html.twig', compact('profiles'));
+    }
+
+    /**
+     * @Route("/test/{id<[0-9]+>}/{lastName}-{firstName}", name="profile.show")
+     */
+    public function show(Profile $profile): Response
+    {
+        return $this->render('home/show.html.twig', compact('profile'));
     }
 }
